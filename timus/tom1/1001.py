afaf1dd81b3da130641d3386ab48488139ef58e0
hopesofbuzzy <nurbaevdaniar0@gmail.com>
2025-11-21 21:32:21 +0500

    timus1001

--- timus/tom1/1001.py
+++ timus/tom1/1001.py
@@ -1,1 +1,4 @@
 import sys
+from math import sqrt
+
+print('\n'.join(reversed([f'{sqrt(num):.4f}' for num in map(int, sys.stdin.read().split())])))
--- timus/tom1/inputflow_test.py
+++ timus/tom1/inputflow_test.py
@@ -1,7 +1,12 @@
 import sys
 
+# Ответ после ввода всех строк (^+Z+Enter)
 lines = sys.stdin.readlines()
 print('This is a line: ', lines)
 
+# Мгновенный ответ после ввода строки
 for line in sys.stdin:
     print(line[:-1])
+
+# Получение всех элементов вне зависимости от количества пробелов или переносов
+print(sys.stdin.read().split())
